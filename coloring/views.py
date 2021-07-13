e4dad26917e2e0058c8d4cacb33eb457b7dae158
Tram Nguyen <tramn@berkeley.edu>
2021-07-13 11:59:08 -0700

    Resized canvas and rotated the color palette

--- coloring/views.py
+++ coloring/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def index(request):
     return render(request, 'coloring/index.html')
+
+def demo(request):
+    return render(request, 'coloring/demo.html')
+    
+def new_interaction(request):
+    return render(request, 'coloring/new_interaction.html')
