8477daba7e7c53e8f63b6f5c8400830a65ff484c
Tram Nguyen <tramn@berkeley.edu>
2021-07-18 18:26:18 -0700

    Pending for final submission

--- frontend/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-// import errorsReducer from "reducers/errorsReducer";
-// import eventsReducer from "reducers/eventsReducer";
-import { combineReducers } from "redux";
-
-export default combineReducers({
-  // errors: errorsReducer,
-  // events: eventsReducer,
-});
